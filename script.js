be25bcc75f88c7cc66b112859f696a8772184e53
Gustavo2003431 <gustavosmacedopf@gmail.com>
2022-03-18 17:08:09 -0300

    TAREFA 4

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function acender(){
-    document.getElementById("lampadapagada").src="lampadaAmarela.png";
+var loop = 0;
+function turnonturnoff(){
+    if(loop %2 == 0){
+        document.getElementById("lampout").src="./assets/yellowlamp.png";
+        document.body.style.background = '#ffff8c';
+    }
+    else{
+        document.getElementById("lampout").src="./assets/graylamp.png";
+        document.body.style.background = '#d4d4d4';
+    }
+    loop++;
 }
 
-function apagar(){
-    document.getElementById("lampadapagada").src="lampadaBranca.png";
+function turnon(){
+    document.getElementById("lampout").src="./assets/yellowlamp.png";
+    document.body.style.background = '#ffff8c';
+}
+
+function turnoff(){
+    document.getElementById("lampout").src="./assets/graylamp.png";
+    document.body.style.background = '#d4d4d4';
 }
